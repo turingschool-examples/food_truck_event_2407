e759cd56fd7d5f48cf2595ce449c94aa528afaeb
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-08-06 09:54:43 -0600

    test: trucks that sell item tesst suite

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -48,4 +48,24 @@ RSpec.describe Event do
             expect(@event.food_truck_names).to eq(expected)
         end
     end
+
+    describe '#food trucks that sell' do
+        it 'returns list of foodtrucks that sell item' do
+            @event.add_food_truck(@food_truck1)    
+            @event.add_food_truck(@food_truck2)    
+            @event.add_food_truck(@food_truck3)
+
+            @food_truck1.stock(@item1, 35)
+            @food_truck1.stock(@item2, 7)    
+            @food_truck2.stock(@item4, 50)    
+            @food_truck2.stock(@item3, 25)
+            @food_truck3.stock(@item1, 65) 
+
+            expected1 = [@food_truck1, @food_truck3]
+            expected2 = [@food_truck2]
+
+            expect(@event.food_trucks_that_sell(@item1)).to eq(expected1)
+            expect(@event.food_trucks_that_sell(@item4)).to eq(expected2)
+        end
+    end
 end
