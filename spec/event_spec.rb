0bb37969a1f8a5c6b171f6cc6ea756b21854f221
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-08-06 09:47:11 -0600

    test: food truck names test suite

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -36,4 +36,16 @@ RSpec.describe Event do
             expect(@event.food_trucks).to eq(expected)
         end
     end
+
+    describe '#food truck names' do
+        it 'returns list of food truck names' do
+            @event.add_food_truck(@food_truck1)
+            @event.add_food_truck(@food_truck2)
+            @event.add_food_truck(@food_truck3)
+
+            expected = ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
+
+            expect(@event.food_truck_names).to eq(expected)
+        end
+    end
 end
