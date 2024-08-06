a86d48ce6435ae3a6236ce4a1aef20cad3423853
Jim Macur <jim.macur@gmail.com>
2024-08-06 09:13:56 -0600

    food truck is initialized and tested

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,2 +1,22 @@
 require './lib/item'
 require './lib/food_truck'
+
+RSpec.configure do |config|
+    config.formatter = :documentation
+end
+
+RSpec.describe FoodTruck do
+    before(:each) do
+        @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+        @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+        @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    end
+    
+    describe 'initialize' do
+        it 'exists' do
+            expect(@food_truck).to be_a FoodTruck
+        end
+        
+    end
+end
