62347c686af19021f0d09db841897c45af498960
John Hill <jphill1989@gmail.com>
2024-08-06 09:36:19 -0600

    Added add_food_truck test

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -6,8 +6,19 @@ RSpec.describe Event do
     before(:each) do
         @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
         @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+        @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+        @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
 
-        @food_truck = FoodTruck.new("Rocky Mountain Pies")
+        @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+        @food_truck1.stock(@item1, 35) 
+        @food_truck1.stock(@item2, 7)
+
+        @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom") 
+        @food_truck2.stock(@item4, 50)
+        @food_truck2.stock(@item3, 25)
+        
+        @food_truck3 = FoodTruck.new("Palisade Peach Shack")
+        @food_truck3.stock(@item1, 65)     
 
         @event = Event.new("South Pearl Street Farmers Market")
     end
@@ -23,6 +34,16 @@ RSpec.describe Event do
 
         it "tracks food trucks; starts with none" do
             expect(@event.food_trucks).to eq []
+        end
+    end
+
+    describe "add_food_truck" do
+        it "adds food trucks to an event" do
+            @event.add_food_truck(@food_truck1)
+            @event.add_food_truck(@food_truck2)
+            @event.add_food_truck(@food_truck3)
+
+            expect(@event.food_trucks).to eq([@food_truck1, @food_truck2, @food_truck3])
         end
     end
 
