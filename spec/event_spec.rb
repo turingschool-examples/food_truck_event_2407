5d99144c44bcfb940b1ef318beb686c8636e0012
Stefan Bloom <stefanjbloom88@gmail.com>
2024-08-06 10:02:26 -0600

    Test for food_truck_names set up

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -36,4 +36,12 @@ RSpec.describe Event do
 
         expect(@event.food_trucks).to eq [@food_truck1, @food_truck2, @food_truck3]
     end
+
+    it "#add_food_truck_names returns the name of the truck in an array" do
+        @event.add_food_truck(@food_truck1)
+        @event.add_food_truck(@food_truck2)
+        @event.add_food_truck(@food_truck3)
+
+        expect(@event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"]) 
+    end
 end
