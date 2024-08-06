17f55b78287c6fa452df9022f272dd75741346de
Jim Macur <jim.macur@gmail.com>
2024-08-06 10:16:54 -0600

    trucks that sell test more robust

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -61,6 +61,7 @@ RSpec.describe Event do
             @event.add_food_truck(@food_truck3)
 
             expect(@event.food_trucks_that_sell(@item1)).to eq [@food_truck1, @food_truck3]
+            expect(@event.food_trucks_that_sell(@item4)).to eq [@food_truck2]
         end
     end
 end
