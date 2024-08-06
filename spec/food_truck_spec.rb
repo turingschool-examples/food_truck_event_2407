8274ac4f405ca75523593179cd7d83580f3c6fa7
Stefan Bloom <stefanjbloom88@gmail.com>
2024-08-06 09:34:31 -0600

    Test refactor for #stock

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -25,5 +25,15 @@ RSpec.describe FoodTruck do
         @food_truck.stock(@item1, 30)
 
         expect(@food_truck.inventory).to eq({@item1 => 30})
+        expect(@food_truck.check_stock(@item1)).to eq 30
+
+        @food_truck.stock(@item1, 25)
+
+        expect(@food_truck.check_stock(@item1)).to eq 55
+
+        @food_truck.stock(@item2, 12)
+
+        expect(@food_truck.inventory).to eq({@item1 => 30, @item2 => 12})
+
     end
 end
