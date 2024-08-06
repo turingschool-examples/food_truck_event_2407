0f50a5926eeddf11ee6dab43eb0064a0eb57fa99
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-08-06 09:59:05 -0600

    test:potential revenue test suite

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -68,4 +68,22 @@ RSpec.describe Event do
             expect(@event.food_trucks_that_sell(@item4)).to eq(expected2)
         end
     end
+
+    describe '#potential revenue' do
+        it 'returns potential revenue for foodtuck' do
+            @event.add_food_truck(@food_truck1)    
+            @event.add_food_truck(@food_truck2)    
+            @event.add_food_truck(@food_truck3)
+
+            @food_truck1.stock(@item1, 35)
+            @food_truck1.stock(@item2, 7)    
+            @food_truck2.stock(@item4, 50)    
+            @food_truck2.stock(@item3, 25)
+            @food_truck3.stock(@item1, 65) 
+
+            expect(@food_truck1.potential_revenue).to eq(148.75)
+            expect(@food_truck2.potential_revenue).to eq(345.00)
+            expect(@food_truck3.potential_revenue).to eq(243.75)
+        end
+    end
 end
