8ef0986730c6341a9583196e5faeffb04c5a31f7
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-08-06 09:10:36 -0600

    test: initialize test suite

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,7 @@
 class Item
 
-    
+    def initialize(info)
+        @name = info[:name]
+        @price = info[:price].to_f
+    end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,10 +3,16 @@ require './lib/item'
 RSpec.describe Item do
     before(:each) do
         @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+        @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     end
     describe '#initialize' do
         it 'exists' do
             expect(@item1).to be_an_instance_of(Item)
+        end
+
+        it 'has attributes' do
+            expect(@item2.name).to eq("Apple Pie (Slice)")
+            expect(@item2.price).to eq(2.50)
         end
     end
 
