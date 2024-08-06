163926a26746c92288e10db9a0f0a73bf03eeef6
Shane Galvin <Sgalvin36@gmail.com>
2024-08-06 09:17:24 -0600

    Add multiple item tracking

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -43,4 +43,15 @@ RSpec.describe FoodTruck do
 
         expect(food_truck.inventory).to eq({item1 => 55})
     end
+
+    it 'stocks seperate items in inventory' do
+        food_truck = FoodTruck.new("Rocky Mountain Pies")
+        item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+        item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+        food_truck.stock(item1, 30)
+        food_truck.stock(item1, 25)
+        food_truck.stock(item2, 12)
+    
+        expect(food_truck.inventory).to eq({item1 => 55, item2 => 12})        
+    end
 end
