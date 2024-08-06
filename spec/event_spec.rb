2efbc04f060d2ff0448e6416218177bd94fcc7e9
Kyle Delaney <khdelaney99@gmail.com>
2024-08-06 12:04:56 -0400

    add test for #food_trucks_that_sell() in Event

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe Item do
         @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
         @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom") 
         @food_truck3 = FoodTruck.new("Palisade Peach Shack")
+        @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+        @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+        @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+        @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
     end
 
     describe '#initialize()' do
@@ -45,6 +49,23 @@ RSpec.describe Item do
             expected = ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
 
             expect(@event.food_truck_names).to eq(expected)
+        end
+    end
+
+    describe '#food_trucks_that_sell()' do
+        it 'returns an array of food truck objects that sell the specified item' do
+            @food_truck1.stock(@item1, 35)
+            @food_truck1.stock(@item2, 7)
+            @food_truck2.stock(@item3, 25)
+            @food_truck2.stock(@item4, 50)
+            @food_truck3.stock(@item1, 65)
+
+            @event.add_food_truck(@food_truck1)
+            @event.add_food_truck(@food_truck2)
+            @event.add_food_truck(@food_truck3)
+
+            expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
+            expect(@event.food_trucks_that_sell(@item4)).to eq([@food_truck2])
         end
     end
 end
