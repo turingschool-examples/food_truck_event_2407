1b3a026b488b7b6a5e72fad5fac4a8c327ff20ca
Renee Messersmith <morganrmessersmith@gmail.com>
2024-08-06 11:53:57 -0400

    feat: food truck names and food trucks that sell

--- lib/event.rb
+++ lib/event.rb
@@ -8,4 +8,23 @@ class Event
     def add_food_truck(food_truck)
         @food_trucks << food_truck
     end
+
+    def food_truck_names
+        names = []
+        @food_trucks.each do |truck|
+            names << truck.name
+        end
+        names
+    end
+
+    def food_trucks_that_sell(item)
+        # @food_trucks[0].inventory.include?(item)
+        selling_trucks = []
+        @food_trucks.each do |truck|
+            if truck.inventory.include?(item)
+                selling_trucks << truck
+            end
+        end
+        selling_trucks
+    end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -38,4 +38,28 @@ RSpec.describe Event do
 
       expect(@event.food_trucks).to eq([@food_truck1, @food_truck2, @food_truck3])
     end
+
+    it 'gives food truck names' do
+        @event.add_food_truck(@food_truck1)
+        @event.add_food_truck(@food_truck2)
+        @event.add_food_truck(@food_truck3)
+
+        expect(@event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+    end
+
+    it 'lists food trucks that sell certain items' do
+      @food_truck1.stock(@item1, 35)  
+      @food_truck1.stock(@item2, 35)
+
+      @food_truck2.stock(@item4, 50)
+      @food_truck2.stock(@item3, 25)
+
+      @food_truck3.stock(@item1, 65)
+
+      @event.add_food_truck(@food_truck1)
+      @event.add_food_truck(@food_truck2)
+      @event.add_food_truck(@food_truck3)
+
+      expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
+    end
 end
