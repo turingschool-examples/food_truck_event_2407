721085abd3b5bd85ee013f9475fbfa3b8ac10e9f
Renee Messersmith <morganrmessersmith@gmail.com>
2024-08-06 13:32:36 -0400

    feat: changed syntax inside enumerables

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,7 +1,7 @@
 class FoodTruck
 
     attr_reader :name, :inventory
-    
+
     def initialize(name)
         @name = name
         @inventory = {}
@@ -10,9 +10,9 @@ class FoodTruck
     def check_stock(item)
         return 0 if !@inventory.include?(item)
         num_stock = nil
-        @inventory.each do |k, v|
-            if k == item
-                num_stock = v
+        @inventory.each do |item, quantity|
+            if item == item
+                num_stock = quantity
             end
         end
         num_stock
@@ -28,8 +28,8 @@ class FoodTruck
 
     def potential_revenue
         revenue = 0.0
-        @inventory.each do |k ,v|
-           revenue += k.price * v
+        @inventory.each do |item, quantity|
+           revenue += item.price * quantity
         end
         revenue
     end
