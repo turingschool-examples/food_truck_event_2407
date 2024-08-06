0cc3f8cf281d226212ad1ab41d4f889657c0b92b
Jim Macur <jim.macur@gmail.com>
2024-08-06 09:09:35 -0600

    item testing is more robust

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -13,5 +13,12 @@ RSpec.describe Item do
             expect(item1.name).to eq 'Peach Pie (Slice)'
             expect(item1.price).to eq "$3.75"
         end
+
+        it 'can have another name and price' do
+            item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+            expect(item2.name).to eq 'Apple Pie (Slice)'
+            expect(item2.price).to eq "$2.50"
+        end
     end
 end
