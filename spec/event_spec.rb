5000a90db7e7bc55bf5563feb5e6309f815d8534
Kyle Delaney <khdelaney99@gmail.com>
2024-08-06 12:33:20 -0400

    add test for #sorted_item_list

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe Item do
         @food_truck3 = FoodTruck.new("Palisade Peach Shack")
         @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
         @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-        @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-        @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+        @item3 = Item.new({name: 'Peach-Raspberry Nice Cream', price: "$5.30"})
+        @item4 = Item.new({name: 'Banana Nice Cream', price: "$4.25"})
     end
 
     describe '#initialize()' do
@@ -66,6 +66,46 @@ RSpec.describe Item do
 
             expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
             expect(@event.food_trucks_that_sell(@item4)).to eq([@food_truck2])
+        end
+    end
+
+    describe '#sorted_item_list' do
+        it 'returns an array of strings that repersents all items in food_trucks array sorted alphabetically' do
+            @food_truck1.stock(@item1, 35)
+            @food_truck1.stock(@item2, 7)
+            @food_truck2.stock(@item3, 25)
+            @food_truck2.stock(@item4, 50)
+
+            @event.add_food_truck(@food_truck1)
+            @event.add_food_truck(@food_truck2)
+            @event.add_food_truck(@food_truck3)
+
+            expected = ['Apple Pie (Slice)', 'Banana Nice Cream', 'Peach Pie (Slice)', 'Peach-Raspberry Nice Cream']
+
+            expect(@event.sorted_item_list).to eq(expected)
+        end
+
+        it 'does not contain any repeating item names' do
+            @food_truck1.stock(@item1, 35)
+            @food_truck1.stock(@item2, 7)
+            @food_truck1.stock(@item3, 25)
+            @food_truck1.stock(@item4, 50)
+            @food_truck2.stock(@item1, 35)
+            @food_truck2.stock(@item2, 7)
+            @food_truck2.stock(@item3, 25)
+            @food_truck2.stock(@item4, 50)
+            @food_truck3.stock(@item1, 35)
+            @food_truck3.stock(@item2, 7)
+            @food_truck3.stock(@item3, 25)
+            @food_truck3.stock(@item4, 50)
+
+            @event.add_food_truck(@food_truck1)
+            @event.add_food_truck(@food_truck2)
+            @event.add_food_truck(@food_truck3)
+
+            expected = ['Apple Pie (Slice)', 'Banana Nice Cream', 'Peach Pie (Slice)', 'Peach-Raspberry Nice Cream']
+
+            expect(@event.sorted_item_list).to eq(expected)
         end
     end
 end
