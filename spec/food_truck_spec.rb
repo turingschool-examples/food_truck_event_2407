a6c66e1f0faf0eb8f539d56bebad56788fcb1924
John Hill <jphill1989@gmail.com>
2024-08-06 09:25:26 -0600

    Added another stock test

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -4,8 +4,9 @@ require './lib/food_truck'
 RSpec.describe FoodTruck do
     before(:each) do
         @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-        @food_truck = FoodTruck.new("Rocky Mountain Pies")
+        @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
 
+        @food_truck = FoodTruck.new("Rocky Mountain Pies")
     end
 
     describe "initialize" do
@@ -34,6 +35,13 @@ RSpec.describe FoodTruck do
             @food_truck.stock(@item1,25)
 
             expect(@food_truck.check_stock(@item1)).to eq 55
+        end
+
+        it "can stock different items" do
+            @food_truck.stock(@item1,30)
+            @food_truck.stock(@item2,25)
+
+            expect(@food_truck.inventory).to eq({@item1 => 30, @item2 => 25})
         end
     end
 end
