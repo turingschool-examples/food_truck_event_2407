d06901a9bee267bb34d5e85bae9b2d053792fed1
Terra Manning <terradmanning@gmail.com>
2024-08-06 10:57:32 -0600

    feat: adds a long  method and a test to show all items

--- lib/event.rb
+++ lib/event.rb
@@ -27,6 +27,45 @@ class Event
     end
     food_trucks_item
   end
+
+
+  # def all_items_in_stock
+  #     all_items = @food_trucks.flat_map do |food_truck|
+  #       food_truck.inventory.keys
+  #     end
+  #     unique_items = all_items.uniq { |item| item.name }
+  #     unique_items.sort_by { |item| item.name }.map(&:name)
+  #   end
+
+  def all_items_in_stock
+    all_items = []
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.each_key do |item|
+        all_items << item
+      end
+    end
+    unique_items = []
+    all_items.each do |item|
+      found_duplicate = false
+      unique_items.each do |unique_item|
+        if unique_item.name == item.name
+          found_duplicate = true
+          break
+        end
+      end
+      unless found_duplicate
+        unique_items << item
+      end
+    end
+    sorted_items = unique_items.sort do |item1, item2|
+      item1.name <=> item2.name
+    end
+    item_names = []
+    sorted_items.each do |item|
+      item_names << item.name
+    end
+    item_names
+  end
 end
 
 
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -5,9 +5,11 @@ require './lib/event'
 RSpec.describe Event do
   before(:each) do
     @event = Event.new("South Pearl Street Farmers Market")  
+
     @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
     @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
     @food_truck3 = FoodTruck.new("Palisade Peach Shack")
+
     @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
@@ -59,5 +61,20 @@ RSpec.describe Event do
     expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
     expect(@event.food_trucks_that_sell(@item4)).to eq([@food_truck2])
   end 
+
+  it 'can return an array with all items the food trucks have in stock' do
+    
+    @food_truck1.stock(@item1, 35)  
+    @food_truck1.stock(@item2, 7)
+    @food_truck2.stock(@item4, 50)    
+    @food_truck2.stock(@item3, 25)
+    @food_truck3.stock(@item1, 65)  
+
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+
+    expect(@event.all_items_in_stock).to eq(["Apple Pie (Slice)", "Banana Nice Cream", "Peach Pie (Slice)", "Peach-Raspberry Nice Cream"])
+  end
 end
 
