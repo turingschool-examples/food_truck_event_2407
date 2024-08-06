f73ffe383e2a76f2b64403cdcd2505337caf6141
Stefan Bloom <stefanjbloom88@gmail.com>
2024-08-06 10:37:40 -0600

    Test: sorted_item_list test setup

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -53,4 +53,20 @@ RSpec.describe Event do
         expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
         expect(@event.food_trucks_that_sell(@item4)).to eq([@food_truck2])
     end
+
+    it '#sorted_item_list returns an array of all FoodTruck item names as a string w/ no duplicates' do
+        @food_truck1.stock(@item1, 35)
+        @food_truck1.stock(@item2, 7)  
+
+        @food_truck2.stock(@item4, 50)
+        @food_truck2.stock(@item3, 25)
+
+        @food_truck3.stock(@item1, 65)
+
+        @event.add_food_truck(@food_truck1)
+        @event.add_food_truck(@food_truck2)
+        @event.add_food_truck(@food_truck3)
+
+        expect(@event.sorted_item_list).to eq(['Apple Pie (Slice)', "Banana Nice Cream", 'Peach Pie (Slice)', "Peach-Raspberry Nice Cream"])
+    end
 end
