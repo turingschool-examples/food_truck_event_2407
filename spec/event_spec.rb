350fa3d4733a52fe143bf8b3fd49d66ae0fff2de
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-08-06 12:30:19 -0400

    Wrote test for foodtrucksthat sell

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -34,4 +34,18 @@ RSpec.describe Event do
 
     expect(@event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
   end
+
+  it 'returns foodtrucks with the specific item' do 
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+    @food_truck3.stock(@item1, 65)
+
+    expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
+    expect(@event.food_trucks_that_sell(@item4)).to eq([@food_truck2])
+  end
 end
