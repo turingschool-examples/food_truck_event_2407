84ecb6487c423cbf2b0a5384ad677183ff3262b7
Renee Messersmith <morganrmessersmith@gmail.com>
2024-08-06 11:35:42 -0400

    feat: better inventory test"

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe FoodTruck do
     before(:each) do
         @food_truck = FoodTruck.new("Rocky Mountain Pies")
         @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+        @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     end
 
     it 'exists' do
@@ -24,5 +25,8 @@ RSpec.describe FoodTruck do
 
         @food_truck.stock(@item1, 25)
         expect(@food_truck.check_stock(@item1)).to eq(55)
+
+        @food_truck.stock(@item2, 12)
+        expect(@food_truck.inventory).to eq({@item1 => 55, @item2 => 12})
     end
 end
