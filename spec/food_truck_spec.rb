6b23204db250eb3f80351e107acfc7a09f63bcfc
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-08-06 09:19:25 -0600

    test: checks stock

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -5,6 +5,8 @@ require './lib/item'
 RSpec.describe FoodTruck do
     before(:each) do
         @food_truck = FoodTruck.new("Rocky Mountain Pies")
+        @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+        @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     end
 
     describe '#initialize' do
@@ -18,5 +20,9 @@ RSpec.describe FoodTruck do
         end
     end
 
-    describe ''
+    describe 'stock' do
+        it 'checks stock' do
+            expect(@food_truck.check_stock(@item1)).to eq(0)
+        end
+    end
 end
