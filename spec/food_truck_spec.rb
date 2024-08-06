f2e5da376caaef71cf18d5d720e0318348ece9e4
Bryan Willett <btwillett2003@gmail.com>
2024-08-06 10:46:59 -0500

    created food_truck class and rspec

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,20 @@
+require './lib/item'
+require './lib/food_truck'
+
+RSpec.configure do |config|
+    config.formatter = :documentation
+end
+
+RSpec.describe FoodTruck do
+    before(:each) do
+        @food_truck = FoodTruck.new("Rocky Mountain Pies")
+        @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+        @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    end
+
+    describe 'Initialize' do
+        it 'exists' do
+            expect(@food_truck).to be_an_instance_of(FoodTruck)
+        end
+    end
+end
