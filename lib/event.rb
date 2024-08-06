e31aabae0c042f5a9a95f05a1e1f4b30eec1be0e
Kyle Delaney <khdelaney99@gmail.com>
2024-08-06 12:06:53 -0400

    pass test for #food_trucks_that_sell() in Event

--- lib/event.rb
+++ lib/event.rb
@@ -13,6 +13,12 @@ class Event
     end
     
     def food_truck_names
-        food_trucks.map { |ft| ft.name}
+        food_trucks.map { |food_truck| food_truck.name}
+    end
+
+    def food_trucks_that_sell(item)
+        @food_trucks.find_all do |food_truck|
+            food_truck.inventory.include?(item)
+        end
     end
 end
