42b0a4f99782d443741433f2639c875e1611d160
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-08-06 11:32:08 -0400

    Created test for stock and method

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,9 +1,23 @@
 class FoodTruck 
 
-  attr_reader :name, :inventory, :check_stock
+  attr_reader :name, :inventory
   def initialize(name)
     @name = name
-    @inventory = {}
-    @check_stock = 0
+    @inventory = {}  
+  end
+
+  def check_stock(item)
+    quantity = 0
+  @inventory.each do |inventory_item, amount|
+    if inventory_item == item
+      quantity = amount 
+    end
+  end
+  # require 'pry'; binding.pry
+  quantity
+  end
+
+  def stock(item, quantity)
+    @inventory[item] = quantity
   end
 end
--- spec/food_truck.rb
+++ spec/food_truck.rb
@@ -6,9 +6,24 @@ RSpec.describe FoodTruck do
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
   end
 
-  it 'exists' do 
+  it 'exists' do
+    expect(@food_truck).to be_an_instance_of(FoodTruck)
+  end
+
+  it 'has a name' do
     expect(@food_truck.name).to eq("Rocky Mountain Pies")
+  end
+
+  it 'has an empty inventory by default' do
     expect(@food_truck.inventory).to eq({})
-    expect(@food_truck.check_stock).to eq(0)
+  end
+
+  it 'can check stock for an item' do
+    expect(@food_truck.check_stock(@item1)).to eq(0)
+  end
+
+  it 'stocks items' do 
+    @food_truck.stock(@item1, 30)
+    expect(@food_truck.check_stock(@item1)).to eq(30)
   end
 end
