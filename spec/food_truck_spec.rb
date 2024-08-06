798b9a66095aaccd425f190bc5e32e9220e5de7d
Jim Macur <jim.macur@gmail.com>
2024-08-06 09:52:58 -0600

    testing can hold multiple items in inventory

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -50,5 +50,18 @@ RSpec.describe FoodTruck do
 
             expect(@food_truck.check_stock(@item1)).to eq 55
         end
+        
+        describe 'inventory' do
+            it 'can hold multiple items in inventory' do
+                @food_truck.stock(@item1, 30)
+                @food_truck.stock(@item1, 25)
+                @food_truck.stock(@item2, 12)
+
+                expect(@food_truck.inventory).to eq({
+                    @item1 => 55,
+                    @item2 => 12
+                })
+            end
+        end
     end
 end
