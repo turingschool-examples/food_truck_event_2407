ea6448f01a4c4502c0487dd4382323b213decfd8
Joel Davalos <jsdavalos@gmail.com>
2024-08-06 11:06:19 -0600

    Refactored before each and tests as well as creat a test for getting a sorted list of items

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -18,6 +18,9 @@ RSpec.describe Event do
     @food_truck2.stock(@item4, 50)
     @food_truck2.stock(@item3, 25)
     @food_truck3.stock(@item1, 65)
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
   end
 
   it 'exists' do
@@ -30,24 +33,19 @@ RSpec.describe Event do
   end
 
   it 'can add food trucks' do 
-    @event.add_food_truck(@food_truck1)
-    @event.add_food_truck(@food_truck2)
-    @event.add_food_truck(@food_truck3)
     expect(@event.food_trucks). to eq([@food_truck1, @food_truck2, @food_truck3])
   end
 
   it 'can return food truck names' do 
-    @event.add_food_truck(@food_truck1)
-    @event.add_food_truck(@food_truck2)
-    @event.add_food_truck(@food_truck3)
     expect(@event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
   end
 
   it 'can find food trucks that sell a given item' do
-    @event.add_food_truck(@food_truck1)
-    @event.add_food_truck(@food_truck2)
-    @event.add_food_truck(@food_truck3)
     expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
     expect(@event.food_trucks_that_sell(@item4)).to eq([@food_truck2])
+  end
+
+  it 'retruns a sorted list of item names' do 
+    expect(@event.sorted_item_list).to eq(['Apple Pie (Slice)', 'Banana Nice Cream', 'Peach Pie (Slice)', 'Peach-Raspberry Nice Cream'])
   end
 end
