1acfc54dc5b9a898c00c9e14ebebbf660621a94c
Joel Davalos <jsdavalos@gmail.com>
2024-08-06 10:01:53 -0600

    Updated can stock tests and foodtruck can now add stock

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -21,13 +21,14 @@ RSpec.describe FoodTruck do
     expect(@food_truck.check_stock(@item1)).to eq(0)
   end
 
-  xit 'can stock items' do 
-    @food_truck.stcok(@item1, 30)
-    expect(@food_truck.ivnentory).to eq({@item1 => 30})
+  it 'can stock items' do 
+    @food_truck.stock(@item1, 30)
+    expect(@food_truck.inventory).to eq({@item1 => 30})
 
     @food_truck.stock(@item1, 25)
     expect(@food_truck.inventory).to eq({@item1 => 55})
 
-    @food_truck.stock(@item2, 12).to eq({@item1 => 55, @item2 => 12})
+    @food_truck.stock(@item2, 12)
+    expect(@food_truck.inventory).to eq({@item1 => 55, @item2 => 12})
   end
 end
