d86b1a19dfba52bb6fa0506900bee31fbd0dc9b3
Bryan Willett <btwillett2003@gmail.com>
2024-08-06 11:35:43 -0500

    added rspec for additional items

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -16,16 +16,22 @@ RSpec.describe Item do
         it 'exists' do
             expect(@item1).to be_an_instance_of(Item)
             expect(@item2).to be_an_instance_of(Item)
+            expect(@item3).to be_an_instance_of(Item)
+            expect(@item4).to be_an_instance_of(Item)
         end
 
         it 'has a name' do
             expect(@item1.name).to eq('Peach Pie (Slice)')
             expect(@item2.name).to eq('Apple Pie (Slice)')
+            expect(@item3.name).to eq("Peach-Raspberry Nice Cream")
+            expect(@item4.name).to eq("Banana Nice Cream")
         end
 
         it 'has a price' do
             expect(@item1.price).to eq(3.75)
             expect(@item2.price).to eq(2.50)
+            expect(@item3.price).to eq(5.30)
+            expect(@item4.price).to eq(4.25)
         end
     end
 end
