88f8ee18adc1ef221dfad11821d10d82d1ff3096
Stefan Bloom <stefanjbloom88@gmail.com>
2024-08-06 09:21:36 -0600

    Test: name and inventory for food truck spec.

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -4,16 +4,14 @@ require './lib/food_truck'
 RSpec.describe FoodTruck do
     before(:each) do
         @food_truck = FoodTruck.new("Rocky Mountain Pies")
-
     end
 
     it 'exists' do
-        @food_truck = FoodTruck.new("Rocky Mountain Pies")
-
         expect(@food_truck).to be_a FoodTruck
     end
 
     it 'has a name and inventory(empty array)' do
-
+        expect(@food_truck.name).to eq("Rocky Mountain Pies")
+        expect(@food_truck.inventory).to eq({})
     end
 end
