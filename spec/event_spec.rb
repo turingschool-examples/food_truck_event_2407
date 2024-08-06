bf9b1448f7a43ded910a23f79089eba628348493
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-08-06 10:28:42 -0600

    feat: sorted item list test suite

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe Event do
         @food_truck3 = FoodTruck.new("Palisade Peach Shack") 
         @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
         @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-        @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-        @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+        @item3 = Item.new({name: 'Peach-Raspberry Nice Cream', price: "$5.30"})
+        @item4 = Item.new({name: 'Banana Nice Cream', price: "$4.25"})
     end
 
     describe '#intialize' do
@@ -90,6 +90,27 @@ RSpec.describe Event do
             expect(@food_truck1.potential_revenue).to eq(148.75)
             expect(@food_truck2.potential_revenue).to eq(345.00)
             expect(@food_truck3.potential_revenue).to eq(243.75)
+        end
+    end
+
+    describe '#sorted item list' do
+        it 'returns unique alphabetical list of all items in stock' do
+            @event.add_food_truck(@food_truck1)    
+            @event.add_food_truck(@food_truck2)    
+            @event.add_food_truck(@food_truck3)
+
+            @food_truck1.stock(@item1, 35)
+            @food_truck1.stock(@item2, 7)    
+            @food_truck2.stock(@item4, 50)    
+            @food_truck2.stock(@item3, 25)
+            @food_truck3.stock(@item1, 65)
+
+            expected = ['Apple Pie (Slice)', 
+                        'Banana Nice Cream', 
+                        'Peach Pie (Slice)', 
+                        'Peach-Raspberry Nice Cream']
+            
+            expect(@event.sorted_item_list).to eq(expected)
         end
     end
 end
