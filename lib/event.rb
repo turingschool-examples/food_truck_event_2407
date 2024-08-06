d93c78f64a70cdd92189ab7ea3762de23011b4b2
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-08-06 09:56:39 -0600

    feat: food truck that sells method

--- lib/event.rb
+++ lib/event.rb
@@ -15,4 +15,10 @@ class Event
             truck.name
         end
     end
+
+    def food_trucks_that_sell(item)
+        @food_trucks.find_all do |truck|
+            truck.inventory.keys.include?(item)
+        end
+    end
 end
