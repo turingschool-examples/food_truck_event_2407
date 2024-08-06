e7a5c50efd7e959d2d9690277146dbe6f3eedf7b
Renee Messersmith <morganrmessersmith@gmail.com>
2024-08-06 13:44:34 -0400

    trying to de-bug

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,5 +1,6 @@
 require './lib/item'
 require './lib/food_truck'
+require './lib/event'
 
 RSpec.describe FoodTruck do
     before(:each) do
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,5 +1,6 @@
 require './lib/item'
 require './lib/food_truck'
+require './lib/event'
 
 RSpec.describe Item do
     before(:each) do
