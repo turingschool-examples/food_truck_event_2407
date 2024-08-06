c319b00da40e3ba98a276598839d4c92484168bc
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-08-06 09:48:21 -0600

    feat: food truck names

--- lib/event.rb
+++ lib/event.rb
@@ -9,4 +9,10 @@ class Event
     def add_food_truck(truck)
         @food_trucks << truck
     end
+
+    def food_truck_names
+        @food_trucks.map do |truck|
+            truck.name
+        end
+    end
 end
