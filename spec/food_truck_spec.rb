6ec8ad5d0e1f4e05b226186f7a655227289882c3
Shane Galvin <Sgalvin36@gmail.com>
2024-08-06 09:14:11 -0600

    Add methods check_stock and stock with tests

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,4 +1,5 @@
 require './lib/food_truck'
+require './lib/item'
 
 RSpec.describe FoodTruck do
     it 'exists' do
@@ -15,5 +16,21 @@ RSpec.describe FoodTruck do
         food_truck = FoodTruck.new("Rocky Mountain Pies")
 
         expect(food_truck.inventory).to eq({})
+    end
+
+    it '#check_stock' do
+        food_truck = FoodTruck.new("Rocky Mountain Pies")
+        item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+        expect(food_truck.check_stock(item1)).to eq 0
+    end
+
+    it '#stock' do
+        food_truck = FoodTruck.new("Rocky Mountain Pies")
+        item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+        food_truck.stock(item1, 30)
+        
+        expect(food_truck.inventory).to eq({item1 => 30})
+        expect(food_truck.check_stock(item1)).to eq 30
     end
 end
