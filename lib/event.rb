88173be7040612a5973283620581d08766fed8d3
Joel Davalos <jsdavalos@gmail.com>
2024-08-06 10:27:09 -0600

    Event class exists and has attributes

---
+++ lib/event.rb
@@ -0,0 +1,10 @@
+require './lib/food_truck'
+
+class Event
+  attr_reader :name, :food_trucks
+
+  def initialize(name)
+    @name = name
+    @food_trucks = []
+  end
+end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,8 +1,9 @@
 require './lib/item'
 require './lib/food_truck'
+require './lib/event'
 
 
-RSpec.describe FoodTruck do 
+RSpec.describe Event do 
   before(:each) do 
     @event = Event.new("South Pearl Street Farmers Market")
     @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
@@ -28,21 +29,21 @@ RSpec.describe FoodTruck do
     expect(@event.food_trucks).to eq([])
   end
 
-  it 'can add food trucks' do 
+  xit 'can add food trucks' do 
     @event.add_food_truck(@food_truck1)
     @event.add_food_truck(@food_truck2)
     @event.add_food_truck(@food_truck3)
     expect(@event.food_trucks). to eq([@food_truck1, @food_truck2, @food_truck3])
   end
 
-  it 'can return food truck names' do 
+  xit 'can return food truck names' do 
     @event.add_food_truck(@food_truck1)
     @event.add_food_truck(@food_truck2)
     @event.add_food_truck(@food_truck3)
     expect(@event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
   end
 
-  it 'can find food trucks that sell a given item' do
+  xit 'can find food trucks that sell a given item' do
     @event.add_food_truck(@food_truck1)
     @event.add_food_truck(@food_truck2)
     @event.add_food_truck(@food_truck3)
