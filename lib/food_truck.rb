b33c542af41719dc1697dcb285ee8c1611f9a79d
John Hill <jphill1989@gmail.com>
2024-08-06 10:03:22 -0600

    Added potential_revenue method

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -15,4 +15,8 @@ class FoodTruck
     def stock(item, quantity)
         @inventory[item] += quantity
     end
+
+    def potential_revenue
+        @inventory.sum { |item, quantity| item.price * quantity }
+    end
 end
