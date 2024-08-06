5d7df9f0407d5f6d75d4126dbbea0d0d75941795
Stefan Bloom <stefanjbloom88@gmail.com>
2024-08-06 09:45:48 -0600

    Feat/Test: event and spec instantiates and has attributes

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe Event do
     before(:each) do
         @event = Event.new("South Pearl Street Farmers Market")
     end
-    
+
+    it 'exists and has attributes' do
+        expect(@event).to be_a(Event)
+        expect(@event.name).to eq("South Pearl Street Farmers Market")
+        expect(@event.food_trucks).to eq([])
+    end
 end
