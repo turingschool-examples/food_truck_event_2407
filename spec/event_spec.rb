4e3dc7b0405dd69d7d434a85799f5a45e0d3823c
Joel Davalos <jsdavalos@gmail.com>
2024-08-06 11:46:00 -0600

    wrote test for returnig overstocked items

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -63,7 +63,7 @@ RSpec.describe Event do
     @event.add_food_truck(@food_truck1)
     @event.add_food_truck(@food_truck2)
     @event.add_food_truck(@food_truck3)
-    
+
     expected = {
       @item1 => { quantity: 100, food_trucks: [@food_truck1, @food_truck3] },
       @item2 => { quantity: 7, food_trucks: [@food_truck1] },
@@ -72,5 +72,12 @@ RSpec.describe Event do
     }
     
     expect(@event.total_inventory).to eq(expected)
+  end
+
+  it 'returns overstocked items' do 
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+    expect(@event.overstocked_items).to eq ([@item1])
   end
 end
